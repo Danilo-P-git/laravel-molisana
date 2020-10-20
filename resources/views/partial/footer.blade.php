9931a1d5db48c9badcc6a17cf554711e38355c89
Danilo Patanè <d.patane98@gmail.com>
2020-10-20 23:23:41 +0200

    Footer done

--- resources/views/partial/footer.blade.php
+++ resources/views/partial/footer.blade.php
@@ -1,3 +1,68 @@
 <footer>
-  <p>QUI VA IL FOOTER</p>
+<div class="container_fluid footer-bg">
+  <div class="container_medium">
+    <div class="row-footer">
+
+
+      <div class="col-footer">
+        <img src="{{asset('images/marchio-sito-test.png')}}" alt="">
+
+        <ul>
+          <li><small>Ragione sociale: La Molisana S.P.A.</small></li>
+          <li><small>Sede legale: Contrada Colle delle Api, </small></li>
+          <li><small>100/A - 86100 - Campobasso (CB)</small></li>
+          <li><small>Pec: lamolisana@pec.it</small></li>
+          <li><small>Tel: +39 0874 4981</small></li>
+          <li><small>Fax: +39 0874 629584</small></li>
+          <li><small>Per segnalazioni scrivere a:</small></li>
+          <li><small>info@lamolisana.it</small></li>
+          <li><small>Per contattare l'area commerciale:</small></li>
+          <li><small>commerciale@lamolisana.it</small></li>
+          <li><small>Per contattare l'area export:</small></li>
+          <li><small>export@lamolisana.it</small></li>
+        </ul>
+      </div>
+
+
+      <div class="col-footer">
+        <h2>PASTIFICIO</h2>
+        <ul>
+          <li>Il Pastificio</li>
+          <li>Grano decorticato a pietra</li>
+          <li>Il Molise c’è</li>
+          <li>Filiera Integrata</li>
+          <li>100 anni di pasta</li>
+          <li>Sartoria della pasta</li>
+          <li>Spaghetto Quadrato</li>
+          <li>Le Persone</li>
+
+        </ul>
+
+        <h2>I PRODOTTI</h2>
+        <ul>
+          <li>Le Classiche</li>
+          <li>Le Integrali</li>
+          <li>Le Speciali</li>
+          <li>Le Biologiche</li>
+          <li>Le Gluten-Free</li>
+          <li>Le Semole</li>
+          <li>Le Extra di Lusso</li>
+
+        </ul>
+      </div>
+      <div class="col-footer">
+        <h2>COLLEZIONE DA CHEF</h2>
+        <ul>
+          <li>Collezione da Chef</li>
+          <li>Grandi Cucine</li>
+          <li>Biologiche</li>
+          <li>Quadrate</li>
+
+        </ul>
+      </div>
+    </div>
+  </div>
+
+</div>
+
 </footer>
